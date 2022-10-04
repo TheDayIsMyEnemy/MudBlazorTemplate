4bcd9a0f9ad369d4becbde36bf6fb7f3a3ced010
TheDayIsMyEnemy <tthedayismyenemy@gmail.com>
2022-10-04 05:42:57 +0300

    Clean up

--- src/Shared/MainLayout.razor.cs
+++ src/Shared/MainLayout.razor.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
 using MudBlazorTemplate.Theme;
 
@@ -11,15 +10,6 @@ namespace MudBlazorTemplate.Shared
         protected DefaultTheme DefaultTheme { get; set; } = new();
         protected bool IsDrawerOpened { get; set; }
         protected bool IsDarkMode { get; set;}
-        protected string? FullName { get; set; }
-        protected string? Role { get; set; }
-        protected char? FirstLetterOfFirstName { get; set; }
-
-        [Inject]
-        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
-
-        //[Inject]
-        //protected UserManager<User> UserManager { get; set; } = null!;
 
         protected void DrawerToggle()
          => IsDrawerOpened = !IsDrawerOpened;
@@ -34,20 +24,6 @@ namespace MudBlazorTemplate.Shared
                 IsDarkMode = await MudThemeProvider.GetSystemPreference();
                 StateHasChanged();
             }
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            //var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-            //var userId = authState.User.GetUserId();
-            //var isAdmin = authState.User.IsInRole(Roles.Admin);
-            //if (userId != null)
-            //{
-            //    var user = await UserManager.FindByIdAsync(userId);
-            //    FirstLetterOfFirstName = user?.FirstName?[0];
-            //    FullName = user?.FullName;
-            //    Role = isAdmin ? Roles.Admin : string.Empty;
-            //}
         }
     }
 }
